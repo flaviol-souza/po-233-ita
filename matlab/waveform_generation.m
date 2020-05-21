4b30cb57668ba5c60ff0b49d1fde8eefa28855af
Flavio Souza <souza@Flavios-Mac-mini.local>
2020-05-21 11:38:15 -0300

    add script and dataset from MatLab

---
+++ matlab/waveform_generation.m
@@ -0,0 +1,90 @@
+modulationTypes = categorical(["BPSK", "QPSK", "8PSK", "16QAM", "64QAM", "PAM4", "GFSK", "CPFSK"]);
+
+numFramesPerModType = 1000;
+percentTrainingSamples = 80;
+percentValidationSamples = 10;
+percentTestSamples = 10;
+
+sps = 8;                % Samples per symbol
+spf = 1024;             % Samples per frame
+symbolsPerFrame = spf / sps;
+fs = 200e3;             % Sample rate
+fc = [902e6 100e6];     % Center frequencies
+
+channel = helperModClassTestChannel(...
+  'SampleRate', fs, ...
+  'SNR', 30, ...
+  'PathDelays', [0 1.8 3.4] / fs, ...
+  'AveragePathGains', [0 -2 -10], ...
+  'KFactor', 4, ...
+  'MaximumDopplerShift', 4, ...
+  'MaximumClockOffset', 5, ...
+  'CenterFrequency', 902e6)
+% Set the random number generator to a known state to be able to regenerate
+% the same frames every time the simulation is run
+rng(1235)
+tic
+
+numModulationTypes = length(modulationTypes);
+
+channelInfo = info(channel);
+transDelay = 50;
+dataDirectory = fullfile(tempdir,"ModClassDataFiles");
+disp("Data file directory is " + dataDirectory)
+
+fileNameRoot = "frame";
+
+% Check if data files exist
+dataFilesExist = false;
+if exist(dataDirectory,'dir')
+  files = ls(fullfile(dataDirectory,sprintf("%s*",fileNameRoot)));
+  if length(files) == numModulationTypes*numFramesPerModType
+    dataFilesExist = true;
+  end
+end
+
+if ~dataFilesExist
+  disp("Generating data and saving in data files...")
+  [success,msg,msgID] = mkdir(dataDirectory);
+  if ~success
+    error(msgID,msg)
+  end
+  for modType = 1:numModulationTypes
+    fprintf('%s - Generating %s frames\n', ...
+      datestr(toc/86400,'HH:MM:SS'), modulationTypes(modType))
+    
+    label = modulationTypes(modType);
+    numSymbols = (numFramesPerModType / sps);
+    dataSrc = helperModClassGetSource(modulationTypes(modType), sps, 2*spf, fs);
+    modulator = helperModClassGetModulator(modulationTypes(modType), sps, fs);
+    if contains(char(modulationTypes(modType)), {'B-FM','DSB-AM','SSB-AM'})
+      % Analog modulation types use a center frequency of 100 MHz
+      channel.CenterFrequency = 100e6;
+    else
+      % Digital modulation types use a center frequency of 902 MHz
+      channel.CenterFrequency = 902e6;
+    end
+    
+    for p=1:numFramesPerModType
+      % Generate random data
+      x = dataSrc();
+      
+      % Modulate
+      y = modulator(x);
+      
+      % Pass through independent channels
+      rxSamples = channel(y);
+      
+      % Remove transients from the beginning, trim to size, and normalize
+      frame = helperModClassFrameGenerator(rxSamples, spf, spf, transDelay, sps);
+      
+      % Save data file
+      fileName = fullfile(dataDirectory,...
+        sprintf("%s%s%03d",fileNameRoot,modulationTypes(modType),p));
+      save(fileName,"frame","label")
+    end
+  end
+else
+  disp("Data files exist. Skip data generation.")
+end
+
